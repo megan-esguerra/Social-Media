642b8d463f6b540c5fe0f5d3f56246f1447bd3f8
Astr-oIT <estacio.gabrielle31@gmail.com>
2025-01-19 20:50:11 +0800

    video add in feed

--- javascript.js
+++ javascript.js
@@ -25,42 +25,7 @@ var swiper = new Swiper(".centered-slide-carousel", {
     }
    });
 
-  //  chat js
-  const chatWindow = document.getElementById('chatWindow');
-    const chatMessages = document.getElementById('chatMessages');
-    const messageInput = document.getElementById('messageInput');
-
-    //chat window
-    function toggleChat() {
-      if (chatWindow.style.display === 'none' || chatWindow.style.display === '') {
-        chatWindow.style.display = 'flex';
-      } else {
-        chatWindow.style.display = 'none';
-      }
-    }
-
-    // Send a message
-    function sendMessage() {
-      const messageText = messageInput.value.trim();
-      if (messageText) {
-        const messageElement = document.createElement('div');
-        messageElement.textContent = messageText;
-        chatMessages.appendChild(messageElement);
-        messageInput.value = '';
-        chatMessages.scrollTop = chatMessages.scrollHeight; 
-      }
-    }
-
-    //dropdown 
-    function toggleDropdown() {
-      const dropdownContent = document.getElementById('dropdownContent');
-      if (dropdownContent.style.display === 'none' || dropdownContent.style.display === '') {
-        dropdownContent.style.display = 'block';
-      } else {
-        dropdownContent.style.display = 'none';
-      }
-    }
-
+   
     // Notification Dropdown
     function toggleNotificationDropdown() {
       const notificationDropdownContent = document.getElementById('notificationDropdownContent');
@@ -70,4 +35,12 @@ var swiper = new Swiper(".centered-slide-carousel", {
         notificationDropdownContent.style.display = 'none';
       }
     }
- 
+    
+    window.addEventListener("load", ()=> {
+      const url = window.location.href;
+      if(url.endsWith('.html')){
+        const newUrl = url.slice(0, -5);
+
+        window.history.pushState({},"", newUrl)
+      }
+    });
